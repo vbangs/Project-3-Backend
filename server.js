c69cc7e64c8a49150e09010b6e7f22a22909374f
vbangs <veronicabangs@gmail.com>
2021-05-22 12:56:54 -0400

    Adding update and show routes

--- server.js
+++ server.js
@@ -53,8 +53,12 @@ app.get("/books", (req, res) => {
 })
 
 // update
-app.put("/books/:id", (req, res) => {
-    res.json("update")
+app.put("/books/:id", async (req, res) => {
+    try {
+      res.json(await Books.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+    } catch (error) {
+      res.status(400).json(error)
+    }
 })
 
 // delete
